beb0c38835c26a4b561a80b168e7fc97b1e22976
Akihiro Kondo <akihyro@users.noreply.github.com>
2015-04-28 16:46:08 +0900

    スケルトンコードを作成

---
+++ src/main/java/akihyro/Luceid.java
@@ -0,0 +1,15 @@
+package akihyro;
+
+import robocode.AdvancedRobot;
+
+/**
+ * ロボット: Luceid.
+ */
+public class Luceid extends AdvancedRobot {
+
+    /** {@inheritDoc} */
+    @Override
+    public void run() {
+    }
+
+}
