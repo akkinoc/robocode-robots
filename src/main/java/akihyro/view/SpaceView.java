46749e33ea2419d9ded399934c5230fb14a56a3f
Akihiro Kondo <akihyro@gmail.com>
2016-01-07 13:48:02 +0900

    何も描画しないビューを実装

---
+++ src/main/java/akihyro/view/SpaceView.java
@@ -0,0 +1,16 @@
+package akihyro.view;
+
+import java.awt.Graphics2D;
+
+/**
+ * 何も描画しないビュー。
+ */
+public class SpaceView extends View {
+
+    /** {@inheritDoc} */
+    @Override
+    public void render(Graphics2D graphics) {
+        // NOP
+    }
+
+}
