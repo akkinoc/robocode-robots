8e040fdc4df3ea368a92aa009b6b70338f542594
Akihiro Kondo <akihyro@gmail.com>
2016-01-09 03:05:28 +0900

    Rect: リファクタ

--- src/main/java/akihyro/geom/Rect.java
+++ src/main/java/akihyro/geom/Rect.java
@@ -1,12 +1,15 @@
 package akihyro.geom;
 
+import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 /**
  * 矩形。
  */
 @Data
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class Rect {
 
     /**
@@ -22,12 +25,58 @@ public class Rect {
     private final Size size;
 
     /**
-     * 右上隅のポイントを取得する。
+     * 矩形を取得する。
      *
-     * @return 右上隅のポイント。
+     * @param bottomLeft 左下隅のポイント。
+     * @param size サイズ。
+     * @return 矩形。
      */
-    public Point topRight() {
-        return new Point(right(), top());
+    public static Rect fromBottomLeft(Point bottomLeft, Size size) {
+        return new Rect(bottomLeft, size);
+    }
+
+    /**
+     * 矩形を取得する。
+     *
+     * @param bottomRight 右下隅のポイント。
+     * @param size サイズ。
+     * @return 矩形。
+     */
+    public static Rect fromBottomRight(@NonNull Point bottomRight, @NonNull Size size) {
+        return fromBottomLeft(bottomRight.offsetX(- size.width()), size);
+    }
+
+    /**
+     * 矩形を取得する。
+     *
+     * @param topLeft 左上隅のポイント。
+     * @param size サイズ。
+     * @return 矩形。
+     */
+    public static Rect fromTopLeft(@NonNull Point topLeft, @NonNull Size size) {
+        return fromBottomLeft(topLeft.offsetY(- size.height()), size);
+    }
+
+    /**
+     * 矩形を取得する。
+     *
+     * @param topRight 右上隅のポイント。
+     * @param size サイズ。
+     * @return 矩形。
+     */
+    public static Rect fromTopRight(@NonNull Point topRight, @NonNull Size size) {
+        return fromBottomLeft(topRight.offset(- size.width(), - size.height()), size);
+    }
+
+    /**
+     * 矩形を取得する。
+     *
+     * @param center 中央のポイント。
+     * @param size サイズ。
+     * @return 矩形。
+     */
+    public static Rect fromCenter(Point center, Size size) {
+        return fromBottomLeft(center.offset(- size.width() / 2.0, - size.height() / 2.0), size);
     }
 
     /**
@@ -67,6 +116,24 @@ public class Rect {
     }
 
     /**
+     * 中央のX座標を取得する。
+     *
+     * @return 中央のX座標。
+     */
+    public double centerX() {
+        return bottomLeft().x() + size().width() / 2.0;
+    }
+
+    /**
+     * 中央のY座標を取得する。
+     *
+     * @return 中央のY座標。
+     */
+    public double centerY() {
+        return bottomLeft().y() + size().height() / 2.0;
+    }
+
+    /**
      * 幅を取得する。
      *
      * @return 幅。
@@ -85,14 +152,55 @@ public class Rect {
     }
 
     /**
+     * 右下隅のポイントを取得する。
+     *
+     * @return 右下隅のポイント。
+     */
+    public Point bottomRight() {
+        return Point.of(right(), bottom());
+    }
+
+    /**
+     * 左上隅のポイントを取得する。
+     *
+     * @return 左上隅のポイント。
+     */
+    public Point topLeft() {
+        return Point.of(left(), top());
+    }
+
+    /**
+     * 右上隅のポイントを取得する。
+     *
+     * @return 右上隅のポイント。
+     */
+    public Point topRight() {
+        return Point.of(right(), top());
+    }
+
+    /**
+     * 中央のポイントを取得する。
+     *
+     * @return 中央のポイント。
+     */
+    public Point center() {
+        return Point.of(centerX(), centerY());
+    }
+
+    /**
      * 近似かどうか判定する。
      *
      * @param rect 矩形。
      * @return 近似かどうか。
      */
     public boolean nears(@NonNull Rect rect) {
-        return bottomLeft().nears(rect.bottomLeft())
-                && size().nears(rect.size());
+        return bottomLeft().nears(rect.bottomLeft()) && size().nears(rect.size());
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String toString() {
+        return "(bottomLeft=" + bottomLeft() + ", size=" + size() + ")";
     }
 
 }
--- src/test/java/akihyro/geom/RectTest.java
+++ src/test/java/akihyro/geom/RectTest.java
@@ -1,37 +1,64 @@
 package akihyro.geom;
 
+import static akihyro.test.assertion.PointAssert.assertThat;
+import static akihyro.test.assertion.SizeAssert.assertThat;
+import static java.lang.Math.nextUp;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.within;
 import org.junit.Test;
-import static robocode.util.Utils.NEAR_DELTA;
-
 /**
  * {@link Rect} のテスト。
  */
 public class RectTest {
 
     /**
-     * {@link Rect#Rect(Point, Size)} をテストする。
+     * {@link Rect#fromBottomLeft(Point, Size)} をテストする。
      */
     @Test
-    public void new_インスタンスを生成できる() {
-        Rect rect = new Rect(
-                new Point(1.2, 3.4),
-                new Size(5.6, 7.8)
-        );
-        assertThat(rect.bottomLeft()).isEqualTo(new Point(1.2, 3.4));
-        assertThat(rect.size()).isEqualTo(new Size(5.6, 7.8));
+    public void fromBottomLeft_矩形を取得できる() {
+        Rect rect = Rect.fromBottomLeft(Point.of(1.2, 3.4), Size.of(5.6, 7.8));
+        assertThat(rect.bottomLeft()).isNear(Point.of(1.2, 3.4));
+        assertThat(rect.size()).isNear(Size.of(5.6, 7.8));
     }
 
     /**
-     * {@link Rect#topRight()} をテストする。
+     * {@link Rect#fromBottomRight(Point, Size)} をテストする。
      */
     @Test
-    public void topRight_右上隅のポイントが取得できる() {
-        Rect rect = new Rect(
-                new Point(1.2, 3.4),
-                new Size(5.6, 7.8)
-        );
-        assertThat(rect.topRight()).isEqualTo(new Point(6.8, 11.2));
+    public void fromBottomRight_矩形を取得できる() {
+        Rect rect = Rect.fromBottomRight(Point.of(1.2, 3.4), Size.of(5.6, 7.8));
+        assertThat(rect.bottomLeft()).isNear(Point.of(-4.4, 3.4));
+        assertThat(rect.size()).isNear(Size.of(5.6, 7.8));
+    }
+
+    /**
+     * {@link Rect#fromTopLeft(Point, Size)} をテストする。
+     */
+    @Test
+    public void fromTopLeft_矩形を取得できる() {
+        Rect rect = Rect.fromTopLeft(Point.of(1.2, 3.4), Size.of(5.6, 7.8));
+        assertThat(rect.bottomLeft()).isNear(Point.of(1.2, -4.4));
+        assertThat(rect.size()).isNear(Size.of(5.6, 7.8));
+    }
+
+    /**
+     * {@link Rect#fromTopRight(Point, Size)} をテストする。
+     */
+    @Test
+    public void fromTopRight_矩形を取得できる() {
+        Rect rect = Rect.fromTopRight(Point.of(1.2, 3.4), Size.of(5.6, 7.8));
+        assertThat(rect.bottomLeft()).isNear(Point.of(-4.4, -4.4));
+        assertThat(rect.size()).isNear(Size.of(5.6, 7.8));
+    }
+
+    /**
+     * {@link Rect#fromCenter(Point, Size)} をテストする。
+     */
+    @Test
+    public void fromCenter_矩形を取得できる() {
+        Rect rect = Rect.fromCenter(Point.of(1.2, 3.4), Size.of(5.6, 7.8));
+        assertThat(rect.bottomLeft()).isNear(Point.of(-1.6, -0.5));
+        assertThat(rect.size()).isNear(Size.of(5.6, 7.8));
     }
 
     /**
@@ -39,11 +66,8 @@ public class RectTest {
      */
     @Test
     public void left_左下隅のX座標が取得できる() {
-        Rect rect = new Rect(
-                new Point(1.2, 3.4),
-                new Size(5.6, 7.8)
-        );
-        assertThat(rect.left()).isEqualTo(1.2);
+        Rect rect = Rect.fromBottomLeft(Point.of(1.2, 3.4), Size.of(5.6, 7.8));
+        assertThat(rect.left()).isCloseTo(1.2, within(0.1));
     }
 
     /**
@@ -51,11 +75,8 @@ public class RectTest {
      */
     @Test
     public void bottom_左下隅のY座標が取得できる() {
-        Rect rect = new Rect(
-                new Point(1.2, 3.4),
-                new Size(5.6, 7.8)
-        );
-        assertThat(rect.bottom()).isEqualTo(3.4);
+        Rect rect = Rect.fromBottomLeft(Point.of(1.2, 3.4), Size.of(5.6, 7.8));
+        assertThat(rect.bottom()).isCloseTo(3.4, within(0.1));
     }
 
     /**
@@ -63,11 +84,8 @@ public class RectTest {
      */
     @Test
     public void right_右上隅のX座標が取得できる() {
-        Rect rect = new Rect(
-                new Point(1.2, 3.4),
-                new Size(5.6, 7.8)
-        );
-        assertThat(rect.right()).isEqualTo(6.8);
+        Rect rect = Rect.fromBottomLeft(Point.of(1.2, 3.4), Size.of(5.6, 7.8));
+        assertThat(rect.right()).isCloseTo(6.8, within(0.1));
     }
 
     /**
@@ -75,11 +93,26 @@ public class RectTest {
      */
     @Test
     public void top_右上隅のY座標が取得できる() {
-        Rect rect = new Rect(
-                new Point(1.2, 3.4),
-                new Size(5.6, 7.8)
-        );
-        assertThat(rect.top()).isEqualTo(11.2);
+        Rect rect = Rect.fromBottomLeft(Point.of(1.2, 3.4), Size.of(5.6, 7.8));
+        assertThat(rect.top()).isCloseTo(11.2, within(0.1));
+    }
+
+    /**
+     * {@link Rect#centerX()} をテストする。
+     */
+    @Test
+    public void centerX_中央のX座標が取得できる() {
+        Rect rect = Rect.fromBottomLeft(Point.of(1.2, 3.4), Size.of(5.6, 7.8));
+        assertThat(rect.centerX()).isCloseTo(4.0, within(0.1));
+    }
+
+    /**
+     * {@link Rect#centerY()} をテストする。
+     */
+    @Test
+    public void centerY_中央のY座標が取得できる() {
+        Rect rect = Rect.fromBottomLeft(Point.of(1.2, 3.4), Size.of(5.6, 7.8));
+        assertThat(rect.centerY()).isCloseTo(7.3, within(0.1));
     }
 
     /**
@@ -87,11 +120,8 @@ public class RectTest {
      */
     @Test
     public void width_幅が取得できる() {
-        Rect rect = new Rect(
-                new Point(1.2, 3.4),
-                new Size(5.6, 7.8)
-        );
-        assertThat(rect.width()).isEqualTo(5.6);
+        Rect rect = Rect.fromBottomLeft(Point.of(1.2, 3.4), Size.of(5.6, 7.8));
+        assertThat(rect.width()).isCloseTo(5.6, within(0.1));
     }
 
     /**
@@ -99,11 +129,44 @@ public class RectTest {
      */
     @Test
     public void height_高さが取得できる() {
-        Rect rect = new Rect(
-                new Point(1.2, 3.4),
-                new Size(5.6, 7.8)
-        );
-        assertThat(rect.height()).isEqualTo(7.8);
+        Rect rect = Rect.fromBottomLeft(Point.of(1.2, 3.4), Size.of(5.6, 7.8));
+        assertThat(rect.height()).isCloseTo(7.8, within(0.1));
+    }
+
+    /**
+     * {@link Rect#bottomRight()} をテストする。
+     */
+    @Test
+    public void bottomRight_右下隅のポイントを取得できる() {
+        Rect rect = Rect.fromBottomLeft(Point.of(1.2, 3.4), Size.of(5.6, 7.8));
+        assertThat(rect.bottomRight()).isNear(Point.of(6.8, 3.4));
+    }
+
+    /**
+     * {@link Rect#topLeft()} をテストする。
+     */
+    @Test
+    public void topLeft_左上隅のポイントを取得できる() {
+        Rect rect = Rect.fromBottomLeft(Point.of(1.2, 3.4), Size.of(5.6, 7.8));
+        assertThat(rect.topLeft()).isNear(Point.of(1.2, 11.2));
+    }
+
+    /**
+     * {@link Rect#topRight()} をテストする。
+     */
+    @Test
+    public void topRight_右上隅のポイントを取得できる() {
+        Rect rect = Rect.fromBottomLeft(Point.of(1.2, 3.4), Size.of(5.6, 7.8));
+        assertThat(rect.topRight()).isNear(Point.of(6.8, 11.2));
+    }
+
+    /**
+     * {@link Rect#center()} をテストする。
+     */
+    @Test
+    public void center_中央のポイントを取得できる() {
+        Rect rect = Rect.fromBottomLeft(Point.of(1.2, 3.4), Size.of(5.6, 7.8));
+        assertThat(rect.center()).isNear(Point.of(4.0, 7.3));
     }
 
     /**
@@ -111,20 +174,8 @@ public class RectTest {
      */
     @Test
     public void nears_閾値内の場合はtrueを返す() {
-        Rect rect1 = new Rect(
-                new Point(1.2, 3.4),
-                new Size(5.6, 7.8)
-        );
-        Rect rect2 = new Rect(
-                new Point(
-                        rect1.bottomLeft().x() + NEAR_DELTA / 2.0,
-                        rect1.bottomLeft().y() + NEAR_DELTA / 2.0
-                ),
-                new Size(
-                        rect1.size().width() + NEAR_DELTA / 2.0,
-                        rect1.size().height() + NEAR_DELTA / 2.0
-                )
-        );
+        Rect rect1 = Rect.fromBottomLeft(Point.of(1.2, 3.4), Size.of(5.6, 7.8));
+        Rect rect2 = Rect.fromBottomLeft(Point.of(nextUp(1.2), nextUp(3.4)), Size.of(nextUp(5.6), nextUp(7.8)));
         assertThat(rect1.nears(rect2)).isTrue();
     }
 
@@ -133,20 +184,8 @@ public class RectTest {
      */
     @Test
     public void nears_左下隅のX座標が閾値外の場合はfalseを返す() {
-        Rect rect1 = new Rect(
-                new Point(1.2, 3.4),
-                new Size(5.6, 7.8)
-        );
-        Rect rect2 = new Rect(
-                new Point(
-                        rect1.bottomLeft().x() + NEAR_DELTA * 2.0,
-                        rect1.bottomLeft().y() + NEAR_DELTA / 2.0
-                ),
-                new Size(
-                        rect1.size().width() + NEAR_DELTA / 2.0,
-                        rect1.size().height() + NEAR_DELTA / 2.0
-                )
-        );
+        Rect rect1 = Rect.fromBottomLeft(Point.of(1.2, 3.4), Size.of(5.6, 7.8));
+        Rect rect2 = Rect.fromBottomLeft(Point.of(1.3, 3.4), Size.of(5.6, 7.8));
         assertThat(rect1.nears(rect2)).isFalse();
     }
 
@@ -155,20 +194,8 @@ public class RectTest {
      */
     @Test
     public void nears_左下隅のY座標が閾値外の場合はfalseを返す() {
-        Rect rect1 = new Rect(
-                new Point(1.2, 3.4),
-                new Size(5.6, 7.8)
-        );
-        Rect rect2 = new Rect(
-                new Point(
-                        rect1.bottomLeft().x() + NEAR_DELTA / 2.0,
-                        rect1.bottomLeft().y() + NEAR_DELTA * 2.0
-                ),
-                new Size(
-                        rect1.size().width() + NEAR_DELTA / 2.0,
-                        rect1.size().height() + NEAR_DELTA / 2.0
-                )
-        );
+        Rect rect1 = Rect.fromBottomLeft(Point.of(1.2, 3.4), Size.of(5.6, 7.8));
+        Rect rect2 = Rect.fromBottomLeft(Point.of(1.2, 3.5), Size.of(5.6, 7.8));
         assertThat(rect1.nears(rect2)).isFalse();
     }
 
@@ -177,20 +204,8 @@ public class RectTest {
      */
     @Test
     public void nears_幅が閾値外の場合はfalseを返す() {
-        Rect rect1 = new Rect(
-                new Point(1.2, 3.4),
-                new Size(5.6, 7.8)
-        );
-        Rect rect2 = new Rect(
-                new Point(
-                        rect1.bottomLeft().x() + NEAR_DELTA / 2.0,
-                        rect1.bottomLeft().y() + NEAR_DELTA / 2.0
-                ),
-                new Size(
-                        rect1.size().width() + NEAR_DELTA * 2.0,
-                        rect1.size().height() + NEAR_DELTA / 2.0
-                )
-        );
+        Rect rect1 = Rect.fromBottomLeft(Point.of(1.2, 3.4), Size.of(5.6, 7.8));
+        Rect rect2 = Rect.fromBottomLeft(Point.of(1.2, 3.4), Size.of(5.7, 7.8));
         assertThat(rect1.nears(rect2)).isFalse();
     }
 
@@ -199,21 +214,18 @@ public class RectTest {
      */
     @Test
     public void nears_高さが閾値外の場合はfalseを返す() {
-        Rect rect1 = new Rect(
-                new Point(1.2, 3.4),
-                new Size(5.6, 7.8)
-        );
-        Rect rect2 = new Rect(
-                new Point(
-                        rect1.bottomLeft().x() + NEAR_DELTA / 2.0,
-                        rect1.bottomLeft().y() + NEAR_DELTA / 2.0
-                ),
-                new Size(
-                        rect1.size().width() + NEAR_DELTA / 2.0,
-                        rect1.size().height() + NEAR_DELTA * 2.0
-                )
-        );
+        Rect rect1 = Rect.fromBottomLeft(Point.of(1.2, 3.4), Size.of(5.6, 7.8));
+        Rect rect2 = Rect.fromBottomLeft(Point.of(1.2, 3.4), Size.of(5.6, 7.9));
         assertThat(rect1.nears(rect2)).isFalse();
+    }
+
+    /**
+     * {@link Rect#toString()} をテストする。
+     */
+    @Test
+    public void toString_文字列に変換できる() {
+        Rect rect = Rect.fromBottomLeft(Point.of(1.2, 3.4), Size.of(5.6, 7.8));
+        assertThat(rect).hasToString("(bottomLeft=(1.2, 3.4), size=(5.6 x 7.8))");
     }
 
 }
