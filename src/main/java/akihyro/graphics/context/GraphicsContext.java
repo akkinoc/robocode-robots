010ed31ed6bcc31eec2fa2b96e531cea4d4d7706
Akihiro Kondo <akihyro@gmail.com>
2016-01-14 21:56:15 +0900

    GraphicsContext: 描画領域全体のサイズを保持

--- src/main/java/akihyro/graphics/context/GraphicsContext.java
+++ src/main/java/akihyro/graphics/context/GraphicsContext.java
@@ -1,27 +1,38 @@
 package akihyro.graphics.context;
 
+import akihyro.geom.Size;
 import java.awt.Graphics2D;
 import lombok.Data;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import robocode.Robot;
 
 /**
  * グラフィックスコンテキスト。
  */
 @Data
+@RequiredArgsConstructor
 public class GraphicsContext {
 
     /**
      * グラフィックス。
      */
+    @NonNull
     private final Graphics2D graphics;
+
+    /**
+     * サイズ。
+     */
+    @NonNull
+    private final Size size;
 
     /**
      * コンストラクタ。
      *
-     * @param graphics グラフィックス。
+     * @param robot ロボット。
      */
-    public GraphicsContext(@NonNull Graphics2D graphics) {
-        this.graphics = graphics;
+    public GraphicsContext(@NonNull Robot robot) {
+        this(robot.getGraphics(), Size.of(robot.getBattleFieldWidth(), robot.getBattleFieldHeight()));
     }
 
 }
