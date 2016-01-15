d00deb4c1366d523dcd0fa61766fd71af5362ed2
Akihiro Kondo <akihyro@gmail.com>
2016-01-15 15:14:22 +0900

    Luceid: モニタをペイント

--- src/main/java/akihyro/Luceid.java
+++ src/main/java/akihyro/Luceid.java
@@ -1,9 +1,28 @@
 package akihyro;
 
+import akihyro.graphics.context.GraphicsContext;
+import akihyro.monitor.Monitor;
+import akihyro.monitor.NameIndicator;
+import java.awt.Graphics2D;
 import robocode.AdvancedRobot;
 
 /**
  * Robot: Luceid.
  */
 public class Luceid extends AdvancedRobot {
+
+    /**
+     * モニタ。
+     */
+    private final Monitor monitor = new Monitor(
+            new NameIndicator(this)
+    );
+
+    /** {@inheritDoc} */
+    @Override
+    public void onPaint(Graphics2D graphics) {
+        GraphicsContext context = new GraphicsContext(this);
+        monitor.view().layout(context).paint(context);
+    }
+
 }
